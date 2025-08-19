9251d5743defe598d7a7290f69d33e371be163df
Veiko Lemberg <iveiks@gmail.com>
2025-08-19 22:50:23 +0300

    created getHumanChoice function

--- javaScript.js
+++ javaScript.js
@@ -9,3 +9,21 @@ function getComputerChoice() {
   }
 }
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+  const humanNumber = parseInt(
+    prompt(`Enter a value:
+    1: Rock
+    2: Paper
+    3: Scissors `)
+  );
+  if (humanNumber === 1) {
+    return 'rock';
+  } else if (humanNumber === 2) {
+    return 'paper';
+  } else if (humanNumber === 3) {
+    return 'scissors';
+  }
+}
+
+console.log(getHumanChoice());
